9b13454898a3bb57e1fe19ddab3df52bed8cd361
Abhishek Kumar <158152076+KumarAbhishek-21@users.noreply.github.com>
2025-03-01 02:48:06 +0530

    Update TaskModel.js

--- backend/Models/TaskModel.js
+++ backend/Models/TaskModel.js
@@ -1,5 +1,5 @@
 // import mongoose from 'mongoose';
-import mongoose from 'mongoose';
+import mongoose from './mongoose';
 
 const taskSchema = new mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }, // Relate task to user
