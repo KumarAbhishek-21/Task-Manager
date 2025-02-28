817a55def4d9d2df6eaae0d892c61c7705e0e058
Abhishek Kumar <158152076+KumarAbhishek-21@users.noreply.github.com>
2025-03-01 01:42:46 +0530

    Update utils.jsx

--- frontend/src/utils.jsx
+++ frontend/src/utils.jsx
@@ -4,6 +4,4 @@ export const notify = (message, type) => {
     toast[type](message);
 }
 
-// export const API_URL = 'https://mern-task-manager-app-deploy-api.vercel.app';
-
-export const API_URL = 'http://localhost:4000/api';
+export const API_URL = "https://task-manager-cxk8.onrender.com";
